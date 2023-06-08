293cbee94c9dcdbd34b985d94751a874e0d0bb53
Lara <larafabian061@gmail.com>
2023-06-07 21:25:55 -0300

    Implementation of automated tests

--- shoppingCart.js
+++ shoppingCart.js
@@ -3,15 +3,26 @@ class ShoppingCart {
   totalPrice = 0
 
   addProduct = (product) => {
+    if(product.price <= 0) throw new Error("Invalid price.")
     this.products.push(product)
+  }
+
+  removeProduct = (id) => {
+    const products = this.products.filter(item => item.id != id)
+    this.products = products
+
+    // Para diminuir o código, o código acima poderia ser reescrito como:
+    // this.products = this.products.filter(item => item.id != id)
+  }
+
+  getTotalPrice = () => {
     let totalPrice = 0
     this.products.forEach(item => {
       totalPrice += item.price
     })
-    this.totalPrice = totalPrice
+    // this.totalPrice = totalPrice
+    return totalPrice
   }
-
-  removeProduct = () => []
 }
 
 module.exports = {
